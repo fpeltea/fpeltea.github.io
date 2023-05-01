17be55e59608175006408b8b8f4c56d8af048f79
fpeltea <38168894+fpeltea@users.noreply.github.com>
2023-05-01 15:56:40 +0300

    Add files via upload

---
+++ precache-manifest.a02fc89b4526fccf9820b45a8a767966.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "19a5c67318ff894b700b",
+    "url": "/static/css/main.e1662181.chunk.css"
+  },
+  {
+    "revision": "19a5c67318ff894b700b",
+    "url": "/static/js/main.19a5c673.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6ae2d017f9fb4f304b15",
+    "url": "/static/js/2.6ae2d017.chunk.js"
+  },
+  {
+    "revision": "86f9383f7f83fca1f16f6dd363b67819",
+    "url": "/static/media/snek.86f9383f.png"
+  },
+  {
+    "revision": "322ab8f0b660938317dd78a5191aa87b",
+    "url": "/static/media/tic.322ab8f0.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "afc5f5187220634275729095f4728e5d",
+    "url": "/index.html"
+  }
+];
